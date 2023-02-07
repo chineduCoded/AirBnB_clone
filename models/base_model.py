c99e0f945c383721b92371885d6c284eb785d0ad
chineduCoded <chinedujohn17@gmail.com>
2023-02-07 13:57:44 -0800

    Create BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -18,9 +18,16 @@ class BaseModel:
 
         Return: none
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    setattr(self, key, value)
+                    if key in ["created_at", "updated_at"]:
+                        setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """
