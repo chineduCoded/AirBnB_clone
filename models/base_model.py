050d2339b263f1354621180a66916ec4b318b54f
chineduCoded <chinedujohn17@gmail.com>
2023-02-07 12:41:08 -0800

    BaseModel class

---
+++ models/base_model.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""Defines BaseModel Class"""
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    """
+    Handles initialization, serialization, and deserialization of instances.
+    """
+    def __init__(self, *args, **kwargs):
+        """
+        Initialize the instance with an id and a created_at timestamp.
+
+        Args:
+        *args: non-keyworded arguments passed
+        **kwargs: keyworded arguments passed
+
+        Return: none
+        """
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """
+        Return string representation of the object
+        """
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        """updates the public instance attribute updated_at with the current datetime"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """
+        returns a dictionary containing all keys/values of the instance
+        """
+        dico = self.__dict__.copy()
+        dico['__class__'] = self.__class__.__name__
+        dico['created_at'] = self.created_at.isoformat()
+        dico['updated_at'] = self.updated_at.isoformat()
+        return dico
