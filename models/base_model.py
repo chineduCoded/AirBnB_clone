93114b912a3382bd3230d8314dfd9259ce700f57
chineduCoded <chinedujohn17@gmail.com>
2023-02-08 23:16:43 -0800

    Store first object

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,7 @@
 """Defines BaseModel Class"""
 import uuid
 from datetime import datetime
+import models
 
 
 class BaseModel:
@@ -28,6 +29,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         """
@@ -36,8 +38,9 @@ class BaseModel:
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        """updates the public instance attribute updated_at with the current datetime"""
+        """updates the public instance attribute with the current datetime"""
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """
---
+++ models/engine/file_storage.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""Defines FileStorage Class"""
+import json
+from models.base_model import BaseModel
+
+
+class FileStorage:
+    """Handles storage and retrieval of instances to and from files."""
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """Return the dictionary of all objects stored."""
+        return FileStorage.__objects
+
+    def new(self, obj):
+        """
+        Args:
+            obj: Add a new object to the storage.
+        """
+        key = type(obj).__name__ + "." + obj.id
+        FileStorage.__objects[key] = obj
+
+    def save(self):
+        """Save all objects to the file."""
+        with open(FileStorage.__file_path, mode='w+', encoding='utf-8') as f:
+            to_save = {key: obj.to_dict() for key, obj in FileStorage.__objects.items()}
+            f.write(json.dumps(to_save))
+
+    def reload(self):
+        """Deserialize JSON file to objects."""
+        try:
+            with open(FileStorage.__file_path, "r", encoding="utf-8") as f:
+                data = json.load(f)
+                for k, v in data.items():
+                    klass = k.split(".")[0]
+                    self.__objects[k] = eval(klass)(**v)
+        except FileNotFoundError:
+            pass
